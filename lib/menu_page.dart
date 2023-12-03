ce8ec6885898bc604fa4d4e12b7ede871251ca5e
antoine gavira <antweeene@antoines-MacBook-Pro.local>
2023-12-03 18:06:22 +0800

    fix: architecture & pubspec error

--- lib/menu_page.dart
+++ lib/menu_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package_details_page.dart';
+import 'Menues.dart';
 
 class MenuPage extends StatelessWidget {
   final String userName;
@@ -34,6 +35,24 @@ class MenuPage extends StatelessWidget {
             Text('View Menu Package Details'),
 
             // Add UI elements for displaying menu package details and images
+            ListView.builder(
+              itemCount: Menues.samples.length,
+              itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                  onTap: () {
+                    // Navigator.push(context,
+                    //   MaterialPageRoute(
+                    //     builder: (context) {
+                    //       return RecipeDetail(recipe: Recipe.samples[index]);
+                    //     },
+                    //   ),
+                    // );
+                  },
+                  child:
+                    buildRecipeCard(Menues.samples[index]),
+                );
+              },
+            ),
 
             SizedBox(height: 20),
 
@@ -50,6 +69,33 @@ class MenuPage extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget buildRecipeCard(Menues menues) {
+    return Card(
+      elevation: 2.0,
+      shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10.0)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Image(image: AssetImage(menues.imgUrl)),
+              const SizedBox(
+              height: 14.0,
+            ),
+            Text(
+              menues.title,
+              style: const TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w700,
+                fontFamily: 'Palatino',
+              ),
+            )
+          ]
+        )
       ),
     );
   }
